a9ee7fd05f4deeed6650b955333e655207e60ef7
Harsha Vardhan <harshavar968@gmail.com>
2023-03-01 17:54:19 +0530

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,15 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// const { contextBridge, ipcRenderer } = require('electron');
+
+// contextBridge.exposeInMainWorld('electron', {
+//   getMenu: () => {
+//     ipcRenderer.send('get-menu');
+
+//     return new Promise(resolve => {
+//       ipcRenderer.once('send-menu', (evt, menu) => {
+//         resolve(menu);
+//       });
+//     });
+//   },
+// });
